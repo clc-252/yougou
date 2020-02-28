60c1f80da478e60ee7bbb4a48f167bd4ba3f02ff
蔡丽纯 <12345.qq.com>
2020-02-28 21:43:22 +0800

    03-实现首页分类导航和楼层数据渲染和样式，以及添加了返回顶部的按钮

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,13 @@ import request from '../../utils/request.js'
 Page({
   data:{
     // 轮播图数据
-    swiperList:[]
+    swiperList:[],
+    // 分类导航数据
+    cateList:[],
+    // 楼层数据
+    floordata:[],
+    // 是否显示返回顶部的按钮
+    isShowTop:false
   },
   /**
  * 生命周期函数--监听页面加载
@@ -19,6 +25,58 @@ Page({
       this.setData({
         swiperList: message
       })
+    })
+
+    // 请求分类导航的数据
+    request({
+      url:'/home/catitems'
+    }).then(res=>{
+      const { message } = res.data
+      const newData=message.map((v,i)=>{
+        if(i===0){
+          v.url='/pages/category/index'
+        }
+        return v
+      })
+      this.setData({
+        cateList: newData
+      })
+    })
+
+    // 请求楼层的数据
+    request({
+      url:'/home/floordata'
+    }).then(res=>{
+      console.log(res)
+      const { message } = res.data
+      this.setData({
+        floordata: message
+      })
+    })
+  },
+  // 回到顶部
+  handleToTop(){
+    wx.pageScrollTo({
+      scrollTop: 0,
+      duration: 300
+    })
+  },
+  // 监听页面的滚动事件
+  onPageScroll(e){
+    const {scrollTop}=e;
+    // 获取当前的状态
+    let isShow = this.data.isShowTop;
+    // 判断
+    if (scrollTop>100){
+      isShow=true
+    }else{
+      isShow = false
+    }
+
+    if (isShow === this.data.isShowTop) return;
+
+    this.setData({
+      isShowTop:isShow
     })
   }
 })
