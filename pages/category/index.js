0f2161a99de355ac92a41fc5d85edd52e7a348d0
蔡丽纯 <12345.qq.com>
2020-02-29 16:52:30 +0800

    05-实现分类页的布局和数据的渲染

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,41 @@
 // pages/category/index.js
+
+// 导入自己封装的request
+import request from '../../utils/request.js'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    current:0,
+    // 分类页面数据列表
+    list:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 请求分类页面的数据
+    request({
+      url:'/categories'
+    }).then(res=>{
+      console.log(res)
+      const {message}=res.data
+      this.setData({
+        list:message
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 切换左边分类菜单栏时触发
+  handleClick(e){
+    // 获取index
+    const {index}=e.currentTarget.dataset
+    // 改变current值
+    this.setData({
+      current:index
+    })
   }
 })
