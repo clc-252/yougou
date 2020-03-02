2d8cf98e1032e90f047f5e74f43448d0ee290f33
蔡丽纯 <12345.qq.com>
2020-03-02 22:13:24 +0800

    08-实现分类页跳转到搜索商品页以及搜索商品页数据的渲染

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,3 +1,6 @@
+// 导入自己封装的request
+import request from '../../utils/request.js'
+
 // pages/search/index.js
 Page({
 
@@ -6,63 +9,44 @@ Page({
    */
   data: {
     // 当前tab栏
-    current:0
+    current:0,
+    // 搜索关键词
+    keyword:'',
+    // 商品列表数据
+    goodsList:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
+   * options是url的参数对象
    */
   onLoad: function (options) {
+    // keyword是url中的参数
+    const {keyword}=options
+    this.setData({
+      keyword:keyword
+    })
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    // 请求商品列表的数据
+    request({
+      url:'/goods/search',
+      data:{
+        query:this.data.keyword,
+        pagenum:1,
+        pagesize:10
+      }
+    }).then(res=>{
+      const {message}=res.data
+      // 修改价格数据 - 保留小数点后两位
+      const goodsList=message.goods.map(v=>{
+        v.goods_price = Number(v.goods_price).toFixed(2)
+        return v;
+      })
+      // 把数据存到goodsList中
+      this.setData({
+        goodsList:message.goods
+      })
+    })
   },
 
   // 切换tab栏时触发
