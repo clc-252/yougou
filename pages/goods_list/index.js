db052b476eedc44f151aaf51fa61b74eb7a37b31
蔡丽纯 <12345.qq.com>
2020-03-02 21:39:50 +0800

    07-实现搜索商品列表页的静态布局

---
+++ pages/goods_list/index.js
@@ -0,0 +1,77 @@
+// pages/search/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // 当前tab栏
+    current:0
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  // 切换tab栏时触发
+  handleClick(e){
+    // 获取index
+    const { index } = e.currentTarget.dataset
+    // 改变current值
+    this.setData({
+      current: index
+    })
+  }
+})
