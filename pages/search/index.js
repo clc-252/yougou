18881180622c0abbf862a3d28a080f3473fa3757
蔡丽纯 <12345.qq.com>
2020-03-03 22:23:10 +0800

    14-添加搜索建议列表和点击取消按钮清空输入框和搜索建议列表

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,3 +1,6 @@
+// 导入自己封装的request
+import request from '../../utils/request.js'
+
 // pages/search/index.js
 Page({
 
@@ -6,7 +9,9 @@ Page({
    */
   data: {
     // 输入框的值
-    inputValue:''
+    inputValue:'',
+    // 搜索关键列表
+    recommend:[]
   },
 
   /**
@@ -22,6 +27,35 @@ Page({
     this.setData({
       inputValue:value
     })
-    console.log(this.data.inputValue)
+
+    // 如果有value值才发起请求
+    if(!value) {
+      // 把搜索列表清空
+      this.setData({
+        recommend: []
+      })
+      return;
+    };
+
+    // 请求输入建议列表
+    request({
+      url:'/goods/qsearch',
+      data:{
+        query:value
+      }
+    }).then(res=>{
+      const {message}=res.data;
+      // 保存到搜索建议的数组
+      this.setData({
+        recommend:message
+      })
+    })
+  },
+  // 点击取消按钮时触发
+  handleCancel(){
+    this.setData({
+      inputValue:'',
+      recommend: []
+    })
   }
 })
