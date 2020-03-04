54c4e17e5c60977b91d6c180014ab028b992e3c4
蔡丽纯 <12345.qq.com>
2020-03-04 16:50:17 +0800

    19-添加商品详情的静态页面

---
+++ pages/goods_detail/index.js
@@ -0,0 +1,20 @@
+// pages/goods_detail/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    banner:[
+      'https://img30.360buyimg.com/da/s590x470_jfs/t1/95835/14/13413/94453/5e5867acEff6177c5/a018f4e0d3b4e841.jpg.webp',
+      'https://img20.360buyimg.com/pop/s590x470_jfs/t1/85749/26/13362/98247/5e579c3eE3e78c862/7c91d105322871af.jpg.webp'
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  }
+})
