87932288f2703531857c602a589a121786bc7ff8
蔡丽纯 <12345.qq.com>
2020-03-05 00:17:38 +0800

    20-实现商品详情页的数据渲染

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,3 +1,6 @@
+// 导入自己封装的request
+import request from '../../utils/request.js'
+
 // pages/goods_detail/index.js
 Page({
 
@@ -5,16 +8,35 @@ Page({
    * 页面的初始数据
    */
   data: {
-    banner:[
-      'https://img30.360buyimg.com/da/s590x470_jfs/t1/95835/14/13413/94453/5e5867acEff6177c5/a018f4e0d3b4e841.jpg.webp',
-      'https://img20.360buyimg.com/pop/s590x470_jfs/t1/85749/26/13362/98247/5e579c3eE3e78c862/7c91d105322871af.jpg.webp'
-    ]
+    detail:{},
+    // 当前tab栏的索引
+    current:0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // console.log(options)
+    // 请求商品详情数据
+    request({
+      url:'/goods/detail',
+      data:{
+        goods_id: options.id
+      }
+    }).then(res=>{
+      // console.log(res)
+      const {message}=res.data
+      this.setData({
+        detail:message
+      })
+    })
+  },
+  // 切换tab栏时触发
+  handleTabClick(e){
+    const {index}=e.currentTarget.dataset
+    this.setData({
+      current:index
+    })
   }
 })
