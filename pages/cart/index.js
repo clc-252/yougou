ecfec7b830522a86485df04562aef7c9d0831830
蔡丽纯 <12345.qq.com>
2020-03-07 00:11:23 +0800

    30-实现修改商品数量（数量为0时删除该商品）

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -8,21 +8,21 @@ Page({
     // 收货信息
     address: {},
     // 购物车商品数据
-    goods:[],
+    goods: [],
     // 总价格
-    allPrice:0
+    allPrice: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     // 将收货地址的数据从本地取出来
     this.setData({
       address: wx.getStorageSync('address') || {}
     })
   },
-  onShow(){
+  onShow() {
     // 因为data和onload只会执行一次，所以要在onshow中执行：每一次打开页面的时候获取一次本地数据
     this.setData({
       goods: wx.getStorageSync('goods') || []
@@ -55,16 +55,75 @@ Page({
   },
 
   // 计算总价格
-  handleAllPrice(){
-    let price=0;
+  handleAllPrice() {
+    let price = 0;
     // 循环goods数组，拿到每个商品的价格
-    this.data.goods.forEach(v=>{
-      price+=v.goods_price*v.number
+    this.data.goods.forEach(v => {
+      price += v.goods_price * v.number
     })
 
     // 修改总价格
     this.setData({
-      allPrice:price
+      allPrice: price
+    })
+  },
+
+
+  // 方法二：添加或减少商品数量
+  handleGoodsNum(e) {
+    // number是1 or -1
+    const {
+      index,
+      number
+    } = e.currentTarget.dataset
+    // 点击当前商品，数量+1
+    this.data.goods[index].number += number;
+
+    // 如果商品数量为0 提示用户是否删除商品
+    if (this.data.goods[index].number === 0) {
+      wx.showModal({
+        title: '提示',
+        content: '是否删除商品',
+        success: (res) => {
+          if (res.confirm) {
+            // 删除当前商品
+            this.data.goods.splice(index, 1)
+
+            // 修改data中goods的数据
+            this.setData({
+              goods: this.data.goods
+            })
+          }
+        }
+      })
+    }
+
+    // 修改data中goods的数据
+    this.setData({
+      goods: this.data.goods
     })
   }
+
+  // 方法一：
+  /* 点击+增加商品数量
+    handleAdd(e) {
+      const { index, number } = e.currentTarget.dataset
+      // 点击当前商品，数量+1
+      this.data.goods[index].number += number;
+      // 修改data中goods的数据
+      this.setData({
+        goods: this.data.goods
+      })
+    },
+
+    减少商品数量
+    handleReduce(e){
+      const { index, number } = e.currentTarget.dataset
+      // 点击当前商品，数量-1
+      this.data.goods[index].number -= number;
+      // 修改data中goods的数据
+      this.setData({
+        goods: this.data.goods
+      })
+    } */
 })
